f3ea35856642c7a6fc544cbb7750a60eb973f938
Aditya <adityaphopale@gmail.com>
2022-09-08 16:20:24 +0530

    wall function with rooms on both side

--- 2R1C_simulation/wall_function.jl
+++ 2R1C_simulation/wall_function.jl
@@ -5,10 +5,13 @@ using ModelingToolkitStandardLibrary.Blocks: Constant
 R1 = 1.0
 R2 = 1.0
 C = 1.0
-Croom = 10.0
-Vin = 1.0
+Croom = 2.0
+C_second_room = 100.0
+Vin1 = 20.0
+Vin2 = 10.0
 
-@named capacitor_room = Capacitor(C=Croom, v_start=Vin)
+@named capacitor_room = Capacitor(C=Croom, v_start=Vin1)
+@named capacitor_second_room = Capacitor(C=C_second_room, v_start=Vin2)
 @named ground = Ground()
 
 @parameters t
@@ -16,8 +19,8 @@ D = Differential(t)
 
 function ThreePort(; name, v1_start = 0.0, v2_start = 0.0, i1_start = 0.0, i2_start = 0.0, i3_start = 0.0)
     @named p1 = Pin()
-    @named n1 = Pin()
-    @named n2 = Pin()
+    @named p2 = Pin()
+    @named n = Pin()
     sts = @variables begin
         v1(t) = v1_start
         v2(t) = v2_start
@@ -26,14 +29,14 @@ function ThreePort(; name, v1_start = 0.0, v2_start = 0.0, i1_start = 0.0, i2_st
         i3(t) = i3_start
         vc(t) = v1_start
     end
-    eqs = [v1 ~ p1.v - n1.v
-           v2 ~ p1.v - n2.v
-           0 ~ p1.i - n1.i - n2.i
+    eqs = [v1 ~ p1.v - n.v
+           v2 ~ p2.v - n.v
+           0 ~ p1.i + p2.i - n.i
            i1 ~ p1.i
-           i2 ~ n1.i
-           i3 ~ n2.i
+           i2 ~ p2.i
+           i3 ~ n.i
           ]
-    return compose(ODESystem(eqs, t, sts, []; name = name), p1, n1, n2)
+    return compose(ODESystem(eqs, t, sts, []; name = name), p1, p2, n)
 end
 
 # function OnePort(; name, v_start = 0.0, i_start = 0.0)
@@ -65,9 +68,10 @@ function wall_2R1C(; name, R1, R2, C)
     # @named capacitor = Capacitor(C=C)
 
     wall_eqs = [
-        v2 ~ R1*i1 + vc
+        v2 ~ R2*i2 + vc
+        v1 ~ R1*i1 + vc
         D(vc) ~ i3/C
-        v1 ~ R1*i1 + R2*i2
+        
         
         # v ~ r1.p.v - r1.n.v
         # connect(oneport.p, r1.p),
@@ -102,18 +106,21 @@ end
 
 eqs = [
     connect(capacitor_room.p, wall.p1)
-    connect(capacitor_room.n, wall.n1, wall.n2, ground.g)
+    connect(capacitor_second_room.p, wall.p2)
+    connect(capacitor_room.n, capacitor_second_room.n, wall.n, ground.g)
+    #connect(capacitor_room.n, wall.p2, wall.n, ground.g)
     ]
  
 # wall.p = capacitor_room.p
 # wall.n = r2.n
 
 
-@named single_layer_wall_model = ODESystem(eqs, t, systems=[wall, capacitor_room, ground])
+@named single_layer_wall_model = ODESystem(eqs, t, systems=[wall, capacitor_room, capacitor_second_room, ground])
 sys = structural_simplify(single_layer_wall_model)
-prob = ODAEProblem(sys, Pair[] , (0, 200.0))
+prob = ODAEProblem(sys, Pair[] , (0, 50.0))
 sol = solve(prob, Tsit5())
 #plot(sol, vars = capacitor_room.v, title = "Single-Layer Wall Model (2R1C) Circuit Demonstration", labels = ["Room Temperature"])
-plot(sol, vars = [capacitor_room.v, wall.vc], title = "Single-Layer Wall Model (2R1C) Circuit Demonstration", labels = ["Room Temperature" "Wall Temperature"])
+plot(sol, vars = [capacitor_room.v, capacitor_second_room.v, wall.vc], title = "Single-Layer Wall Model (2R1C) Circuit Demonstration", labels = ["Room Temperature" "Second Room Temperature" "Wall Temperature"])
+#plot(sol, vars = [capacitor_room.v, wall.vc], title = "Single-Layer Wall Model (2R1C) Circuit Demonstration", labels = ["Room Temperature" "Wall Temperature"])
 
 savefig("plot.png")
