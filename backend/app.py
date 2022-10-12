88ea0abf91d673e2b099c2119ad4c5738800b63b
boriskova_liza <ge26kok@mytum.de>
2022-10-12 18:11:18 +0200

    mock rest of the pipeline

--- backend/app.py
+++ backend/app.py
@@ -5,12 +5,16 @@ import flask
 import json
 from flask_cors import CORS
 
+import matplotlib.pyplot as plt
 import detection_tools as dt
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=["POST"]) # new endpoint
+rooms_image = None
+labels = None
+
+@app.route('/image', methods=["POST"])
 def callback():
     print("user endpoint reached...")
     if request.method == "POST":
@@ -22,7 +26,28 @@ def callback():
         floor_plan = dt.msg_to_png(received_data)
         rooms_image, labels = dt.detect_rooms(floor_plan)
 
-        # send image to front to display
+        return flask.Response(response=return_data, status=201)
+
+
+@app.route('/rooms', methods=["POST"])
+def callback():
+    print("user endpoint reached...")
+    if request.method == "POST":
+        received_data = request.get_json()
+        return_data = {
+            # picture generation from yolo converted to msg
+        }
+
+        return flask.Response(response=return_data, status=201)
+
+@app.route('/rc', methods=["POST"])
+def callback():
+    print("user endpoint reached...")
+    if request.method == "POST":
+        received_data = request.get_json()
+        return_data = {
+            # picture generation from rc converted to msg
+        }
 
         return flask.Response(response=return_data, status=201)
 
--- frontend/image_handler.js
+++ frontend/image_handler.js
@@ -21,6 +21,20 @@ image_input.addEventListener("change", function() {
   reader.readAsDataURL(this.files[0]);
 });
 
+// get and show an image from user
+floor_plan.addEventListener("", function() {
+  // this element is supposed to first call requestRooms
+  // and then listen to ACK, from which retrieve the image 
+  // and plot it
+});
+
+// get and show an image from user
+rc_output.addEventListener("", function() {
+  // this element is supposed to first call requestRC
+  // and then listen to ACK, from which retrieve the image 
+  // and plot it
+});
+
 function approvePlan(){
   sendImage();
   // redirect to the next page
@@ -30,8 +44,28 @@ function sendImage(){
   console.log("Sending the image");
   xhr = getXmlHttpRequestObject();
   // asynchronous request
-  xhr.open("POST", "http://localhost:6969/", true);
+  xhr.open("POST", "http://localhost:6969/image", true);
   xhr.setRequestHeader("Content-Type", "application/json");
   // Send image over the network
   xhr.send(JSON.stringify(uploaded_image));
+}
+
+function requestRooms(){
+  console.log("Sending the rooms request");
+  xhr = getXmlHttpRequestObject();
+  // asynchronous request
+  xhr.open("POST", "http://localhost:6969/rooms", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  // Send image over the network
+  xhr.send();
+}
+
+function requestRC(){
+  console.log("Sending the modelling result request");
+  xhr = getXmlHttpRequestObject();
+  // asynchronous request
+  xhr.open("POST", "http://localhost:6969/rc", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  // Send image over the network
+  xhr.send();
 }
