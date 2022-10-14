0d001a5b8d702fcf64a47fe25edb055cfc01d37e
Piyush Karki <piyush.karki25@gmail.com>
2022-10-14 17:26:21 +0200

    setup env works now

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,4 @@
-pwd=
+pwd=(pwd)
 sudo rm -r /home/project12321
 sudo mkdir /home/project12321
 cd /home/project12321
@@ -7,8 +7,11 @@ cd -
 sudo cp test_run.ipynb /home/project12321/yolov5
 sudo cp test.png /home/project12321/yolov5
 sudo cp best.pt /home/project12321/yolov5
+sudo cp arial.ttf /home/project12321/yolov5
 cd -
 cd yolov5
-sudo mkdir -p runs/train/exp9
-sudo cp ./best.pt /home/project12321/yolov5/runs/train/exp9
-jupyter-notebook
+sudo mkdir -p runs/train/exp9/weights
+sudo cp ./best.pt /home/project12321/yolov5/runs/train/exp9/weights
+sudo chmod +rwx ../yolov5
+sudo -E env "PATH=$PATH" jupyter-notebook --allow-root
+
