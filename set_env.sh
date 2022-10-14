ad137db565185602210f076c09b42a0876e8f3bd
Piyush Karki <piyush.karki25@gmail.com>
2022-10-14 12:29:10 +0200

    environment cleared

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,4 @@
-pwd=
+pwd=(pwd)
 sudo rm -r /home/project12321
 sudo mkdir /home/project12321
 cd /home/project12321
@@ -9,6 +9,7 @@ sudo cp test.png /home/project12321/yolov5
 sudo cp best.pt /home/project12321/yolov5
 cd -
 cd yolov5
-sudo mkdir -p runs/train/exp9
-sudo cp ./best.pt /home/project12321/yolov5/runs/train/exp9
-jupyter-notebook
+sudo mkdir -p runs/train/exp9/weights
+sudo cp ./best.pt /home/project12321/yolov5/runs/train/exp9/weights
+chown -R user-id:group-id .
+jupyter-notebook --allow-root
